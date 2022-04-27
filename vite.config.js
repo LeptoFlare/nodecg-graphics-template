699b3b08fc7cbb6bc2ff455686c1c884a98011ab
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2022-04-27 16:20:41 -0400

    Replace react with preact

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
-import react from "@vitejs/plugin-react"
+import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
   base: "./",
   build: {
     target: "chrome95",
-    outDir: "graphics",
     sourcemap: true,
+    outDir: "graphics",
     rollupOptions: {
       input: {
         break: resolve(__dirname, "break.html"),
